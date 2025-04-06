57677ffba5fcc1b6287f8e771e3a0363406bbe22
bonczbe <bonczbe@gmail.com>
2025-04-06 23:17:50 +0200

    switched to apex chart on device history, need to work on it next week

--- app/Filament/Resources/DeviceHistoryResource/Widgets/DeviceWaterChart.php
+++ app/Filament/Resources/DeviceHistoryResource/Widgets/DeviceWaterChart.php
@@ -2,88 +2,124 @@
 
 namespace App\Filament\Resources\DeviceHistoryResource\Widgets;
 
+use App\Models\Device;
 use App\Models\DeviceHistory;
-use Filament\Widgets\ChartWidget;
+use Carbon\Carbon;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
 
-class DeviceWaterChart extends ChartWidget
+class DeviceWaterChart extends ApexChartWidget
 {
-    protected static ?string $heading = 'Device Water Level';
+    protected static ?string $chartId = 'deviceWaterChart';
 
-    protected function getFilters(): ?array
+    protected function getHeading(): string
     {
-        return [
-            'today' => 'Today',
-            'week' => 'Last week',
-            'month' => 'Last month',
-            'year' => 'This year',
-        ];
+        $deviceId = $this->filterFormData['name'] ?? null;
+
+        if ($deviceId) {
+            $device = Device::find($deviceId);
+
+            return 'Device Water Chart - '.($device?->name ?? 'Unknown Device');
+        }
+
+        return 'Device Water Chart - no specified';
     }
 
-    protected function getData(): array
+    protected function getFormSchema(): array
     {
-        $query = DeviceHistory::query();
-
-        $filter = $this->filter;
-
-        match ($filter) {
-            'today' => $query->whereDate('created_at', now()),
-            'week' => $query->whereBetween('created_at', [now()->subWeek(), now()]),
-            'month' => $query->whereBetween('created_at', [now()->subMonth(), now()]),
-            'year' => $query->whereBetween('created_at', [now()->subYear(), now()]),
-            default => null,
-        };
-
-        $results = $query->orderBy('created_at')->get();
-
         return [
-            'datasets' => [
-                [
-                    'label' => 'Water Level',
-                    'data' => $results->pluck('water_level')->toArray(),
-                ],
-            ],
-            'labels' => $results->pluck('created_at')->map(fn ($date) => $date->format('M d H:i'))->toArray(),
+            Select::make('name')
+                ->options(
+                    Device::all()
+                        ->pluck('name', 'id')
+                )
+                ->default(Device::first()->id),
+            DatePicker::make('date_start')
+                ->default(now()->subMonth()->toDateString()),
+            DatePicker::make('date_end')
+                ->default(now()->toDateString()),
         ];
-    }
-
-    protected function getType(): string
-    {
-        return 'line';
     }
 
     protected function getOptions(): array
     {
+        $deviceId = $this->filterFormData['name'];
+        $dateStart = $this->filterFormData['date_start'] ?? now()->subMonth()->toDateString();
+        $dateEnd = $this->filterFormData['date_end'] ?? now()->toDateString();
+
+        $query = DeviceHistory::query()
+            ->whereBetween('created_at', [$dateStart, $dateEnd]);
+
+        if ($deviceId) {
+            $query->where('device_id', $deviceId);
+        } else {
+            $query->where('id', -1);
+        }
+
+        $results = $query
+            ->selectRaw('DATE(created_at) as date, SUM(water_level) as total_water_level')
+            ->groupBy('date')
+            ->orderBy('date')
+            ->get();
+
+        $data = $results->pluck('total_water_level')
+            ->map(fn ($value) => round($value, 2))
+            ->toArray();
+
+        $labels = $results->pluck('date')
+            ->map(fn ($date) => Carbon::parse($date)->format('Y-m-d'))
+            ->toArray();
+
         return [
-            'scales' => [
-                'y' => [
-                    'beginAtZero' => true,
-                    'title' => [
-                        'display' => true,
-                        'text' => 'Water Level (%)',
-                    ],
+            'chart' => [
+                'type' => 'line',
+                'height' => 300,
+                'toolbar' => [
+                    'show' => true,
                 ],
-                'x' => [
-                    'title' => [
-                        'display' => true,
-                        'text' => 'Date',
-                    ],
-                ],
-            ],
-            'plugins' => [
-                'legend' => [
-                    'display' => true,
-                    'position' => 'top',
-                ],
-                'tooltip' => [
+                'zoom' => [
                     'enabled' => true,
-                    'mode' => 'index',
-                    'intersect' => false,
                 ],
             ],
-            'interaction' => [
-                'mode' => 'nearest',
-                'axis' => 'x',
-                'intersect' => false,
+            'series' => [
+                [
+                    'name' => 'Water Level (%)',
+                    'data' => $data,
+                ],
+            ],
+            'xaxis' => [
+                'categories' => $labels,
+                'labels' => [
+                    'rotate' => -45,
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+                'title' => [
+                    'text' => 'Date',
+                ],
+            ],
+            'yaxis' => [
+                'title' => [
+                    'text' => 'Water Level (%)',
+                ],
+                'labels' => [
+                    'style' => [
+                        'fontFamily' => 'inherit',
+                    ],
+                ],
+                'min' => 0,
+            ],
+            'colors' => ['#f59e0b'],
+            'stroke' => [
+                'curve' => 'smooth',
+            ],
+            'tooltip' => [
+                'enabled' => true,
+            ],
+            'legend' => [
+                'position' => 'top',
             ],
         ];
     }
